70b041f8a4df07201b77cde156ca678d9320c38d
Mithun <mithun.yadav@webskitters.com>
2026-02-02 08:30:47 +0530

    initilize repository

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotendv from "dotenv";
+
+dotendv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
